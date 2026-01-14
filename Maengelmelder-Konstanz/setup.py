faae1c786a7802c5ba5b9f179b6ddc8da9cb333a
Toby2906 <143411959+Toby2906@users.noreply.github.com>
2026-01-14 07:37:13 +0100

    Delete Maengelmelder-Konstanz directory

--- Maengelmelder-Konstanz/setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name="maengelmelder-konstanz",
-    version="1.0.0",
-    packages=find_packages(exclude=["tests"]),
-    python_requires=">=3.8",
-)
--- Maengelmelder-Konstanz/tests/test_fetch.py
+++
@@ -1,5 +0,0 @@
-from src.fetch import load_data_from_csv
-
-def test_load_csv(sample_csv):
-    texts = load_data_from_csv(sample_csv)
-    assert len(texts) >= 0
